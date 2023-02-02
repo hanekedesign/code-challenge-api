d9fc65aa19de877dbdf92063da2e77095f7fb8b0
Mark Smith <msmith46234@yahoo.com>
2023-02-02 08:03:33 -0500

    update for heroku

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,9 @@ const core_1 = require("@nestjs/core");
 const platform_fastify_1 = require("@nestjs/platform-fastify");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
+    const PORT = process.env.PORT || 3000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter());
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
